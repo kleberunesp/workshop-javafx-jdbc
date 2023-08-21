cb868c1323140b06fe8338079d194b18bdf081a7
kleberunesp <kleber.c.silva@unesp.br>
2023-08-21 14:47:42 -0300

    Department ComboBox

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -112,6 +112,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
 			dialogStage.showAndWait();
 		}
 		catch (IOException e) {
+			e.printStackTrace();
 			Alerts.showAlert("IO Exception", "Error Loading View", e.getMessage(),AlertType.ERROR);
 		}
 	}
